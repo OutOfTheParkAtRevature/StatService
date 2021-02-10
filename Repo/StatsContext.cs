3c97a207fa2d56e5a5f5abc0e36bf5ac76d39be7
Duangboudda <tongpan78@hotmail.com>
2021-02-10 04:16:36 -0800

    add migration and update DB

--- Repo/StatsContext.cs
+++ Repo/StatsContext.cs
@@ -19,5 +19,14 @@ namespace Repository
 
         public StatsContext() { }
         public StatsContext(DbContextOptions<StatsContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+            {
+                options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=AppStore;Trusted_Connection = True;");
+            }
+
+        }
     }
 }
--- StatService/Startup.cs
+++ StatService/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Repopository;
+//using Repopository;
 using Repository;
 using Service;
 using System;

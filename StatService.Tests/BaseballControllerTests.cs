0554016f6e489958b978717b2c684b516b8413ae
Travis Martin <tamarti2@ncsu.edu>
2021-02-21 10:52:17 -0500

    Wrote controller tests
    PlayerGame Controller methods won't work because they aren't set up for a composite key

--- StatService.Tests/BaseballControllerTests.cs
+++ StatService.Tests/BaseballControllerTests.cs
@@ -20,7 +20,7 @@ namespace StatService.Tests
         public async void TestForGetBaseballStatistics()
         {
             var options = new DbContextOptionsBuilder<StatsContext>()
-            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .UseInMemoryDatabase(databaseName: "p3GetBaseballStatistics")
             .Options;
 
             using (var context = new StatsContext(options))
@@ -146,6 +146,147 @@ namespace StatService.Tests
                 Assert.Equal(3.1114M, sportStastic.Value.ERA);
                 Assert.Equal(32, sportStastic.Value.StrikeOuts);
                 Assert.Equal(25, sportStastic.Value.Saves);
+            }
+        }
+
+        /// <summary>
+        /// Tests the PutBaseballStatistic method of BaseballController
+        /// </summary>
+        [Fact]
+        public async void TestForPutBaseballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                Logic l = new Logic(r, new NullLogger<Repo>());
+                BaseballStatisticsController controller = new BaseballStatisticsController(context, l);
+                var baseballStatistics = new BaseballStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    BattingAve = 4.7689M,
+                    Runs = 17,
+                    RBI = 2.3156M,
+                    Hits = 13,
+                    Steals = 23,
+                    ERA = 3.1114M,
+                    StrikeOuts = 32,
+                    Saves = 25
+                };
+                var baseballStatistics2 = new BaseballStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    BattingAve = 5.1432M,
+                    Runs = 33,
+                    RBI = 3.1111M,
+                    Hits = 21,
+                    Steals = 9,
+                    ERA = 1.1234M,
+                    StrikeOuts = 25,
+                    Saves = 32
+                };
+                var sportStatistic = await controller.PutBaseballStatistic(baseballStatistics.StatLineID, baseballStatistics2);
+                Assert.IsAssignableFrom<BadRequestResult>((sportStatistic as BadRequestResult));
+                //var baseballStatistics3 = new BaseballStatistic()
+                //{
+                //    StatLineID = baseballStatistics.StatLineID,
+                //    BattingAve = 5.1432M,
+                //    Runs = 33,
+                //    RBI = 3.1111M,
+                //    Hits = 21,
+                //    Steals = 9,
+                //    ERA = 1.1234M,
+                //    StrikeOuts = 25,
+                //    Saves = 32
+                //};
+                var sportStatistic2 = await controller.PutBaseballStatistic(baseballStatistics.StatLineID, baseballStatistics);
+                Assert.IsAssignableFrom<NotFoundResult>((sportStatistic2 as NotFoundResult));
+                r.BaseballStatistics.Add(baseballStatistics);
+                await r.CommitSave();
+                var sportStatistic3 = await controller.PutBaseballStatistic(baseballStatistics.StatLineID, baseballStatistics);
+                Assert.IsAssignableFrom<NoContentResult>((sportStatistic3 as NoContentResult));
+
+            }
+        }
+
+        /// <summary>
+        /// Tests the PostBaseballStatistic method of BaseballController
+        /// </summary>
+        [Fact]
+        public async void TestForPostBaseballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                Logic l = new Logic(r, new NullLogger<Repo>());
+                BaseballStatisticsController controller = new BaseballStatisticsController(context, l);
+                var baseballStatistics = new BaseballStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    BattingAve = 4.7689M,
+                    Runs = 17,
+                    RBI = 2.3156M,
+                    Hits = 13,
+                    Steals = 23,
+                    ERA = 3.1114M,
+                    StrikeOuts = 32,
+                    Saves = 25
+                };
+                var sportStatistic = await controller.PostBaseballStatistic(baseballStatistics);
+                Assert.IsAssignableFrom<CreatedAtActionResult>(sportStatistic.Result as CreatedAtActionResult);
+            }
+        }
+
+        /// <summary>
+        /// Tests the DeleteBaseballStatistic method of BaseballController
+        /// </summary>
+        [Fact]
+        public async void TestForDeleteBaseballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3BaseballController")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                Logic l = new Logic(r, new NullLogger<Repo>());
+                BaseballStatisticsController controller = new BaseballStatisticsController(context, l);
+                var baseballStatistics = new BaseballStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    BattingAve = 4.7689M,
+                    Runs = 17,
+                    RBI = 2.3156M,
+                    Hits = 13,
+                    Steals = 23,
+                    ERA = 3.1114M,
+                    StrikeOuts = 32,
+                    Saves = 25
+                };
+                r.BaseballStatistics.Add(baseballStatistics);
+                await r.CommitSave();
+
+                var sportStatistic = await controller.DeleteBaseballStatistic(Guid.NewGuid());
+                Assert.IsAssignableFrom<NotFoundResult>(sportStatistic as NotFoundResult);
+                var sportStatistic2 = await controller.DeleteBaseballStatistic(baseballStatistics.StatLineID);
+                Assert.IsAssignableFrom<NoContentResult>(sportStatistic2 as NoContentResult);
             }
         }
     }
---
+++ StatService.Tests/FootballControllerTests.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using Models;
+using Repository;
+using StatService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StatService.Tests
+{
+    public class FootballControllerTests
+    {
+        /// <summary>
+        /// Tests the GetFootballStatistics() method of FootballController
+        /// </summary>
+        [Fact]
+        public async void TestForGetFootballStatistics()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3GetFootballStatistics")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                FootBallStatisticsController controller = new FootBallStatisticsController(context);
+                var footballStatistics = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+                r.FootballStatistics.Add(footballStatistics);
+                await r.CommitSave();
+
+                var sportStatisticList = await controller.GetFootballStatistics();
+                var convertStatisticList = (List<FootBallStatistic>)sportStatisticList.Value;
+                Assert.Contains<FootBallStatistic>(convertStatisticList[0], context.FootballStatistics);
+            }
+        }
+
+        /// <summary>
+        /// Tests the GetFootballStatistic(id) method of FootballController
+        /// </summary>
+        [Fact]
+        public async void TestForGetFootballStatisticById()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                FootBallStatisticsController controller = new FootBallStatisticsController(context);
+                var footballStatistics = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+                r.FootballStatistics.Add(footballStatistics);
+                await r.CommitSave();
+
+                var sportStatistic = await controller.GetFootBallStatistic(Guid.NewGuid());
+                Assert.IsAssignableFrom<NotFoundResult>((sportStatistic.Result as NotFoundResult));
+                var sportStatistic2 = await controller.GetFootBallStatistic(footballStatistics.StatLineID);
+                Assert.True(sportStatistic2.Value.Sacks.Equals(19));
+            }
+        }
+
+        /// <summary>
+        /// Tests the PutFootballStatistic() method of FootballController
+        /// </summary>
+        [Fact]
+        public async void TestForPutFootballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                FootBallStatisticsController controller = new FootBallStatisticsController(context);
+                var footballStatistics = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+                var footballStatistics2 = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+
+                var sportStatistic = await controller.PutFootBallStatistic(footballStatistics.StatLineID, footballStatistics2);
+                Assert.IsAssignableFrom<BadRequestResult>((sportStatistic as BadRequestResult));
+
+                var sportStatistic2 = await controller.PutFootBallStatistic(footballStatistics.StatLineID, footballStatistics);
+                Assert.IsAssignableFrom<NotFoundResult>((sportStatistic2 as NotFoundResult));
+
+                r.FootballStatistics.Add(footballStatistics);
+                await r.CommitSave();
+
+                var sportStatistic3 = await controller.PutFootBallStatistic(footballStatistics.StatLineID, footballStatistics);
+                Assert.IsAssignableFrom<NoContentResult>((sportStatistic3 as NoContentResult));
+            }
+        }
+
+        /// <summary>
+        /// Tests the PostFootballStatistic() method of FootballController
+        /// </summary>
+        [Fact]
+        public async void TestForPostFootballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3StatService")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                FootBallStatisticsController controller = new FootBallStatisticsController(context);
+                var footballStatistics = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+
+                var sportStatistic = await controller.PostFootBallStatistic(footballStatistics);
+                Assert.IsAssignableFrom<CreatedAtActionResult>(sportStatistic.Result as CreatedAtActionResult);
+            }
+        }
+
+        /// <summary>
+        /// Tests the DeleteFootballStatistic() method of FootballController
+        /// </summary>
+        [Fact]
+        public async void TestForDeleteFootballStatistic()
+        {
+            var options = new DbContextOptionsBuilder<StatsContext>()
+            .UseInMemoryDatabase(databaseName: "p3FootballController")
+            .Options;
+
+            using (var context = new StatsContext(options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                Repo r = new Repo(context, new NullLogger<Repo>());
+                FootBallStatisticsController controller = new FootBallStatisticsController(context);
+                var footballStatistics = new FootBallStatistic()
+                {
+                    StatLineID = Guid.NewGuid(),
+                    YardsRec = 25,
+                    YardsRun = 72,
+                    Sacks = 19,
+                    Turnovers = 13,
+                    Plays = 31,
+                    FirstDownCons = 35,
+                    PossessionTime = 21,
+                    Penalties = 13
+                };
+
+                r.FootballStatistics.Add(footballStatistics);
+                await r.CommitSave();
+
+                var sportStatistic = await controller.DeleteFootBallStatistic(Guid.NewGuid());
+                Assert.IsAssignableFrom<NotFoundResult>(sportStatistic as NotFoundResult);
+                var sportStatistic2 = await controller.DeleteFootBallStatistic(footballStatistics.StatLineID);
+                Assert.IsAssignableFrom<NoContentResult>(sportStatistic2 as NoContentResult);
+            }
+        }
+    }
+}
